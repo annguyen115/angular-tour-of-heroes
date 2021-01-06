62a2a00d6e0364b2157cd8b71a955c1b4907ecce
An Nguyen <an.nguyen@fractal.vn>
2021-01-06 13:35:20 +0700

    Add dashboard component, add router

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,11 +13,26 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Partial<Hero>;
 
-  constructor() {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {
     this.hero = {};
   }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  getHero(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.heroService.getHero(+id).subscribe(hero => this.hero = hero || {});
+    }
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,7 +14,6 @@ export class HeroesComponent implements OnInit {
 
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService,
   ) {
     this.heroes = [];
     this.selectedHero = {};
@@ -23,11 +21,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
